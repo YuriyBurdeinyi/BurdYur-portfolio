6c6394a34b765c4f371e1e145c16d8df807713fc
YuriyBurdeinyi <50658375+YuriyBurdeinyi@users.noreply.github.com>
2019-06-11 17:35:25 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,37 +1,26 @@
-// функція для мобільного меню
-menu_icon.onclick = function menuFunction(){
-	var x = document.getElementById("mytopnav")
-
-	if (x.className === "topnav") {
-		x.className += " responsive";
+menu_icon.onclick = function (){
+	var topNav = document.getElementById("topnav-responsive")
+	if (topNav.className === 'topnav') {
+		topNav.className += ' responsive';
 	} else {
-		x.className = "topnav";
+		topNav.className = 'topnav';
 	}
 }
 
 
-// функція для прокрутки догори
-$(document).ready(function(){
-  $('body').append('<a href="#" id="go-top" title="to up">&#9650;</a>');
-});
-
 $(function() {
- $.fn.scrollToTop = function() {
-  $(this).hide().removeAttr("href");
-  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
-  var scrollDiv = $(this);
-  $(window).scroll(function() {
-   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
-   else $(scrollDiv).fadeIn("slow")
-  });
-  $(this).click(function() {
-   $("html, body").animate({scrollTop: 0}, "slow")
-  })
- }
+	$('body').append('<a href="#" id="go-top" title="to up">&#9650;</a>');
+ 	$.fn.scrollToTop = function() {
+  	$(this).hide().removeAttr('href');
+ 	 if ($(window).scrollTop() >= '250') $(this).fadeIn('slow')
+  	var scrollDiv = $(this);
+  	$(window).scroll(function() {
+ 	if ($(window).scrollTop() <= '250') $(scrollDiv).fadeOut('slow')
+    else $(scrollDiv).fadeIn('slow')
+ 	});
+    $(this).click(function() {
+    $('html, body').animate({scrollTop: 0}, "slow")
+    })
+    } 
+    $('#go-top').scrollToTop();
 });
-
-$(function() {
- $("#go-top").scrollToTop();
-});
-
-
